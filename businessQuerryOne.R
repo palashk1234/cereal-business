3bbc37298eef60931f3ee910a4ae3d236dca2d4f
root <root@localhost.localdomain>
2018-04-17 23:55:35 +0530

    Business querry one

---
+++ businessQuerryOne.R
@@ -0,0 +1,38 @@
+dataset <- read.csv('cereal.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = rating ~ calories+protein+fat
+               +sodium+fiber+carbo+sugars+potass+vitamins+shelf+weight+cups, data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# # Visualising the Training set results
+# library(ggplot2)
+# ggplot() +
+#   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+#              colour = 'red') +
+#   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+#             colour = 'blue') +
+#   ggtitle('Salary vs Experience (Training set)') +
+#   xlab('Years of experience') +
+#   ylab('Salary')
+# 
+# # Visualising the Test set results
+# library(ggplot2)
+# ggplot() +
+#   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+#              colour = 'red') +
+#   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+#             colour = 'blue') +
+#   ggtitle('Salary vs Experience (Test set)') +
+#   xlab('Years of experience') +
+#   ylab('Salary')
